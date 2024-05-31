ecb70c8e289f1ec6569eff16e12441cd5ef3b3d0
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2024-05-31 16:28:01 +0200

    id-4 resolved

--- src/main/java/control/Vendita.java
+++ src/main/java/control/Vendita.java
@@ -41,76 +41,83 @@ public class Vendita extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ProductBean product = new ProductBean();
-		product.setEmail((String) request.getSession().getAttribute("email"));
-		
-		 String UPLOAD_DIRECTORY = request.getServletContext().getRealPath("/")+"img/productIMG/";
-		    //process only if its multipart content
-		    if(ServletFileUpload.isMultipartContent(request)) {
-		        try {
-		            List<FileItem> multiparts = new ServletFileUpload(
-		                                     new DiskFileItemFactory()).parseRequest(new ServletRequestContext(request));
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductBean product = new ProductBean();
+        product.setEmail(htmlEscape((String) request.getSession().getAttribute("email")));
+        
+        String UPLOAD_DIRECTORY = request.getServletContext().getRealPath("/") + "img/productIMG/";
+        // process only if it's multipart content
+        if (ServletFileUpload.isMultipartContent(request)) {
+            try {
+                List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(new ServletRequestContext(request));
 
-		            for(FileItem item : multiparts){
-		                if(!item.isFormField()){
-		                    String name = new File(item.getName()).getName();
-		                    item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
-		                    product.setImmagine(name);
-		                }
-		                else {
-		                	if (item.getFieldName().compareTo("nome") == 0) {
-		                		product.setNome(item.getString());
-		                	}
-		                	else if (item.getFieldName().compareTo("prezzo") == 0) {
-		                		product.setPrezzo(Double.parseDouble(item.getString()));
-		                	}
-		                	else if (item.getFieldName().compareTo("spedizione") == 0) {
-		                		product.setSpedizione(Double.parseDouble(item.getString()));
-		                	}
-		                	else if (item.getFieldName().compareTo("tipologia") == 0) {
-		                		product.setTipologia(item.getString());
-		                	}
-							else if (item.getFieldName().compareTo("tag") == 0) {
-								product.setTag(item.getString());
-							}
-							else if (item.getFieldName().compareTo("descrizione") == 0) {
-		                		product.setDescrizione(item.getString());
-		                	}
-		                }
-		            }
+                for (FileItem item : multiparts) {
+                    if (!item.isFormField()) {
+                        String name = new File(item.getName()).getName();
+                        item.write(new File(UPLOAD_DIRECTORY + File.separator + name));
+                        product.setImmagine(htmlEscape(name));
+                    } else {
+                        String fieldName = item.getFieldName();
+                        String fieldValue = htmlEscape(item.getString());
 
-		           //File uploaded successfully
-		           request.setAttribute("message", "File Uploaded Successfully");
-		           
-		        } catch (Exception ex) {
-		           
-		        }          
+                        if (fieldName.compareTo("nome") == 0) {
+                            product.setNome(fieldValue);
+                        } else if (fieldName.compareTo("prezzo") == 0) {
+                            product.setPrezzo(Double.parseDouble(fieldValue));
+                        } else if (fieldName.compareTo("spedizione") == 0) {
+                            product.setSpedizione(Double.parseDouble(fieldValue));
+                        } else if (fieldName.compareTo("tipologia") == 0) {
+                            product.setTipologia(fieldValue);
+                        } else if (fieldName.compareTo("tag") == 0) {
+                            product.setTag(fieldValue);
+                        } else if (fieldName.compareTo("descrizione") == 0) {
+                            product.setDescrizione(fieldValue);
+                        }
+                    }
+                }
 
-		    }
-		    else{
-		        request.setAttribute("message",
-		                             "Sorry this Servlet only handles file upload request");
-		       
-		    }
-		    ProductModel model = new ProductModel();
-		    try {
-				model.doSave(product);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    request.getSession().setAttribute("refreshProduct", true);
-		    request.getRequestDispatcher("/index.jsp").forward(request, response);
-		}
-		    
+                // File uploaded successfully
+                request.setAttribute("message", "File Uploaded Successfully");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+            } catch (Exception ex) {
+                request.setAttribute("message", "File Upload Failed due to " + ex);
+            }
+        } else {
+            request.setAttribute("message", "Sorry this Servlet only handles file upload request");
+        }
+
+        ProductModel model = new ProductModel();
+        try {
+            model.doSave(product);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.getSession().setAttribute("refreshProduct", true);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+	
+    /**
+     * Escapes HTML characters to prevent XSS attacks.
+     * @param input the input string
+     * @return the escaped string
+     */
+    private String htmlEscape(String input) {
+    	if (input == null) {
+    		return null;
+    	}
+    	return input.replace("&", "&amp;")
+    			.replace("<", "&lt;")
+    			.replace(">", "&gt;")
+    			.replace("\"", "&quot;")
+    			.replace("'", "&#x27;");
+    }
 
 }
