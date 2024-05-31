43c855b7b45ce9c81e7ef13b5c0d660c44944792
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2024-05-31 19:25:35 +0200

    id-5 resolved

---
+++ src/main/java/security/SecurityHeadersFilter.java
@@ -0,0 +1,52 @@
+package security;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class SecurityHeadersFilter
+ */
+@WebFilter("/SecurityHeadersFilter")
+public class SecurityHeadersFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SecurityHeadersFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if (response instanceof HttpServletResponse) {
+            HttpServletResponse httpResp = (HttpServletResponse) response;
+            httpResp.setHeader("X-Frame-Options", "DENY");
+            httpResp.setHeader("Content-Security-Policy", "frame-ancestors 'self'");
+        }
+        chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
